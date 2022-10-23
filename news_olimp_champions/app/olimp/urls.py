9edffee4f835ced42092b887250bde82d4530bca
Latyshevdp <Latyshevdp.contacts@yandex.ru>
2022-10-23 10:26:45 +0500

    Comments_admin

--- news_olimp_champions/app/olimp/urls.py
+++ news_olimp_champions/app/olimp/urls.py
@@ -6,18 +6,28 @@ from app.olimp.views import \
     FeedBackCreateView
 
 urlpatterns = [
+    # Маршрут ссылки на все записи новостей
     path('', StandList.as_view(), name='stand_list_view'),
+    # Маршрут ссылки на конкретную детализированную запись новости
     path('<int:pk>/', StandDetail.as_view(), name='stand_detail_view'),
+    # Маршрут ссылки на добавление новости
     path(r'add_stand/', StandCreateView.as_view(), name='add_stand'),
 
+    # Маршрут ссылки на все записи спортсмены
     path('sportsman/', SportsmanList.as_view(), name='sportsman'),
+    # Маршрут ссылки на конкретную детализированную запись спортсмена
     path('sportsman/<int:pk>', SportsmanDetail.as_view(), name='sportsman_detail_view'),
+    # Маршрут ссылки на добавление спортсмена
     path('sportsman/add_sportsman', SportsmanCreateView.as_view(), name='add_sportsman'),
 
+    # Маршрут ссылки на все записи тренера
     path('trener/', TrenerList.as_view(), name='trener'),
+    # Маршрут ссылки на конкретную детализированную запись тренера
     path('trener/<int:pk>', TrenermanDetail.as_view(), name='trener_detail_view'),
+    # Маршрут ссылки на добавление тренера
     path('trener/add_trener', TrenerCreateView.as_view(), name='add_trener'),
 
+    # Маршрут ссылки на добавление обратной связи
     path('feedback/', FeedBackCreateView.as_view(), name='feedback'),
 
 ]
